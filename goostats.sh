e8084a08aa5b422feeec90f6c142f395b0c83929
abnasirr <a.musa@student.curtin.edu.au>
2025-10-07 14:15:25 +0800

    Update goostats.sh
    add calc of max

--- goostats.sh
+++ goostats.sh
@@ -3,4 +3,4 @@ fname=$1
 echo "working with ${fname}"
 echo  "Welcome to Nelle  stats script" 
 #Compute min/max/range of values in a file
-min=$( cat ${fname} | sort | head -1)
+max =$( cat ${fname} | sort | tail -1)
